105582fae17aee0f22bdb6ddbf097537876e0f89
dima <dmitreyg@yahoo.com>
2024-09-10 07:45:37 -0400

    Initial commit

---
+++ src/app/product.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+  private apiUrl = 'https://dummyjson.com/products';
+
+  constructor(private http: HttpClient) {}
+
+  getProducts(): Observable<any> {
+
+    console.log('get products');
+    return this.http.get<any>(this.apiUrl);
+  }
+}
---
+++ src/app/product/product.component.ts
@@ -0,0 +1,28 @@
+
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../product.service';
+import { CommonModule } from '@angular/common';
+
+
+@Component({
+  selector: 'app-product',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './product.component.html',
+  styleUrl: './product.component.css'
+})
+export class ProductComponent implements OnInit {
+
+  products: any;
+
+  constructor(private productService: ProductService) {}
+
+  ngOnInit(): void {
+
+    console.log('product cimponent in init');
+    this.productService.getProducts().subscribe((response: any) => {
+      this.products = response.products;
+    });
+  }
+
+}
